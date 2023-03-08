93b79e3b83d38281ca16304b5186d6e9584fa433
Jason Burgess <jason.nzd@gmail.com>
2023-03-08 16:23:36 +1300

    refactored code and improved readme

--- HttpImageTrigger.cs
+++ HttpImageTrigger.cs
@@ -14,101 +14,152 @@ using Amazon.S3.Model;
 using Amazon.Runtime;
 using ImageMagick;
 
-public static class HttpImageTrigger
+public static class ImageMakeTransparent
 {
-    // Singletons for httpclient and s3
-    static HttpClient httpclient;
+    // Client singleton and variables for S3
     static IAmazonS3 s3client;
-    static string s3bucket = "countdown-thumbs";
+    static readonly string s3bucket = "supermarketimages";
+    static readonly RegionEndpoint region = RegionEndpoint.APSoutheast2;
 
-    [FunctionName("HttpImageTrigger")]
+    [FunctionName("ImageMakeTransparentTrigger")]
     public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-        ILogger log)
+
+    // Trigger off http GET requests
+    [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+    ILogger log)
     {
         // Get query parameters from http trigger
-        string id = req.Query["id"];
+        string fileName = req.Query["fileName"];
         string url = req.Query["url"];
+        // string widthString = req.Query["width"];
+        // string heightString = req.Query["height"];
+        // string qualityString = req.Query["quality"];
 
         // If query parameters are not valid, return error Message
-        if (id == null || url == null || id.Length < 3 || !url.Contains("http"))
+        if (fileName == null || url == null || fileName.Length < 3 || !url.Contains("http"))
         {
             return new OkObjectResult(
-                "This function requires 'id' and 'url' query parameters\n " +
-                "example: /HttpImageTrigger?id=12345&url=http://domain.com/filename.jpg"
+                "This function requires 'fileName' and 'url' query parameters\n " +
+                "example: /HttpImageTrigger?fileName=12345&url=http://domain.com/filename.jpg"
             );
         }
 
-        // Establish connection to S3
-        var connectResponse = ConnectToS3();
+        // Todo: handle width, height, quality queries
+        // ValidateHeightWidth();
+
+        // Build a consolidated message string, which will be added to with other functions
+        string consolidatedMsg = "";
+
+
+        // Establish connection to S3, return if unable to connect
+        var connectResponse = await ConnectToS3();
         if (!connectResponse.Succeeded)
             return new BadRequestObjectResult(connectResponse.Message);
 
-        // Init httpclient, s3client, out stream
-        httpclient = new HttpClient();
-        Stream outStream = new MemoryStream();
 
-        // Derive filename to be written to S3 from id
-        string fileName = id + ".webp";
-
-        // Check S3 if file already exists
-        var existsResponse = await imageAlreadyExistsOnS3(fileName);
+        // Check S3 if file already exists, return if already exists
+        fileName = CleanFileName(fileName);
+        var existsResponse = await ImageAlreadyExistsOnS3(fileName, log);
         if (existsResponse.Succeeded) return new OkObjectResult(existsResponse.Message);
 
-        // If it doesn't exist, download from url as bytes[]
-        byte[] downloadedBytes;
-        try
-        {
-            downloadedBytes = await httpclient!.GetByteArrayAsync(url);
-            log.LogWarning(url);
-        }
-        catch (System.Exception e)
-        {
-            return new BadRequestObjectResult(url + " was unable to be downloaded" +
-            e.Message + e.ToString());
-        }
 
-        string consolidatedMessage = "";
+        // Download from url, return if unsuccessful
+        var downloadResponse = await DownloadImageUrlToStream(url, log);
+        if (downloadResponse.Succeeded)
+            consolidatedMsg += downloadResponse.Message;
+        else
+            return new BadRequestObjectResult(downloadResponse.Message);
 
-        // Load stream into ImageMagick, convert to transparent 180px webp image
-        var imResponse = MakeImageTransparent(downloadedBytes, outStream, log);
+
+        // Load stream into ImageMagick for conversion, return if unsuccessful
+        Stream outStream = new MemoryStream();
+        var imResponse = MakeImageTransparent(downloadResponse.bytePayload, outStream, log);
         if (imResponse.Succeeded)
-            consolidatedMessage += imResponse.Message + "\n";
+            consolidatedMsg += imResponse.Message + "\n\n";
         else
             return new BadRequestObjectResult(imResponse.Message);
 
-        // Upload stream to S3
+
+        // Upload stream to S3, return final consolidated message, or unsuccessful upload msg
         var s3Response = await UploadStreamToS3(fileName, outStream, log);
         if (s3Response.Succeeded)
-            return new OkObjectResult(consolidatedMessage + s3Response.Message);
+            return new OkObjectResult(consolidatedMsg + s3Response.Message);
         else
             return new BadRequestObjectResult(s3Response.Message);
     }
 
-    private static Response ConnectToS3()
+    private static async Task<Response> DownloadImageUrlToStream(string url, ILogger log)
     {
         try
         {
-            // Connect to S3 using credentials from env
-            string accessKey = Environment.GetEnvironmentVariable("AWSAccessKey");
-            string secretKey = Environment.GetEnvironmentVariable("AWSSecretKey");
-            BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-            s3client = new AmazonS3Client(credentials, RegionEndpoint.APSoutheast2);
+            // Download image using httpclient
+            HttpClient httpclient = new HttpClient();
+            byte[] downloadedBytes = await httpclient!.GetByteArrayAsync(url);
 
-            return new Response(true, "");
+            // Log filename and dispose httpclient
+            log.LogWarning(ExtractFileNameFromUrl(url));
+            httpclient.Dispose();
+
+            // Return Response with success, message, and byte[] payload
+            return new Response(
+                true,
+                $"Original Image: {ExtractFileNameFromUrl(url)}\n" +
+                    $"Size: {printFileSize(downloadedBytes.LongLength)}\n\n",
+                downloadedBytes
+            );
         }
-        catch (System.Exception)
+        catch (System.Exception e)
         {
             return new Response(
                 false,
-                "Unable to connect to S3. Check credentials were set as environment settings:\n" +
-                "\"AWSAccessKey\": \"<your aws access key>\",\n" +
-                "\"AWSSecretKey\": \"<your aws secret key>\""
+                url + " was unable to be downloaded" + e.ToString()
             );
         }
     }
 
-    private static Response MakeImageTransparent(byte[] input, Stream outStream, ILogger log)
+    private static async Task<Response> ConnectToS3()
+    {
+        try
+        {
+            // Connect to S3 using credentials from env
+            string accessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY");
+            string secretKey = Environment.GetEnvironmentVariable("AWS_SECRET_KEY");
+            BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+            s3client = new AmazonS3Client(credentials, region);
+
+            // Test S3 connection is valid by attempting to get test.webp
+            await s3client.GetBucketLocationAsync(s3bucket);
+
+            // If no exceptions have been thrown, we have successfully connected to S3
+            return new Response(true);
+        }
+        catch (System.Exception e)
+        {
+            if (e.Message.StartsWith("The specified bucket does not exist"))
+            {
+                return new Response(
+                    false,
+                    $"Unable to connect to S3. The bucket: {s3bucket} does not exist and needs " +
+                    "to be manually created."
+                );
+            }
+            return new Response(
+                false,
+                "Unable to connect to S3. Check credentials were set as environment settings:\n" +
+                "\"AWS_ACCESS_KEY\": \"<your aws access key>\",\n" +
+                "\"AWS_SECRET_KEY\": \"<your aws secret key>\"\n" +
+                e.Message + "\n" + e.ToString()
+            );
+        }
+    }
+
+    private static Response MakeImageTransparent(
+        byte[] input,
+        Stream outStream,
+        ILogger log,
+        int width = 200,
+        int height = 200,
+        int quality = 75)
     {
         try
         {
@@ -123,9 +174,9 @@ public static class HttpImageTrigger
                 image.FloodFill(MagickColors.Transparent, 1, 1);
                 image.Shave(1, 1);
 
-                // Sets the output format to 200x200 webp
-                image.Scale(200, 200);
-                image.Quality = 75;
+                // Sets the output format to a default of 200x200 webp
+                image.Scale(width, height);
+                image.Quality = quality;
                 image.Format = MagickFormat.WebP;
 
                 // Write the image to outStream
@@ -136,8 +187,8 @@ public static class HttpImageTrigger
 
                 return new Response(
                     true,
-                    "Image resized - original = " + printFileSize(input.LongLength) +
-                    " - thumbnail = " + printFileSize(imageByteLength)
+                    "Converted to Transparent Image with dimensions: " +
+                    $"{width}x{height} \nSize: {printFileSize(imageByteLength)}"
                 );
             }
         }
@@ -148,10 +199,67 @@ public static class HttpImageTrigger
         }
     }
 
+    // Try to extract a filename from a url, returns back the full url if unsuccessful
+    private static string ExtractFileNameFromUrl(string url)
+    {
+        try
+        {
+            string[] splitString = url.Split('/');
+            string lastSection = splitString[splitString.Length - 1];
+            if (lastSection.Contains('?'))
+            {
+                lastSection = lastSection.Substring(0, lastSection.IndexOf('?'));
+            }
+            return lastSection;
+        }
+        catch (System.Exception)
+        {
+            return url;
+        }
+    }
+
+    private static void ValidateHeightWidth(string dimensionString)
+    {
+        // if (dimensionString == null || dimensionString == "") return 0;
+
+        // try
+        // {
+        //     int dimension = int.Parse(dimensionString);
+        //     if (dimension < 20 || dimension > 4000) 
+        //         throw new Exception();
+
+        //     return dimension;
+        // }
+        // catch (System.Exception)
+        // {
+
+        //     return 0;
+        // }
+    }
+
+
+    private static string CleanFileName(string fileName)
+    {
+        // If fileName contains a .extension other than webp, replace it with webp
+        if (fileName.Contains('.') && !fileName.ToLower().EndsWith(".webp"))
+        {
+            string[] splitFileName = fileName.Split('.');
+            string originalExtension = splitFileName[splitFileName.Length - 1].ToLower();
+            fileName = fileName.Replace(originalExtension, "webp");
+        }
+        else
+        {
+            // If fileName has no .extension, add .webp
+            fileName = fileName += ".webp";
+        }
+        return fileName;
+    }
+
     private static async Task<Response> UploadStreamToS3(string fileName, Stream stream, ILogger log)
     {
         try
         {
+
             var putRequest = new PutObjectRequest
             {
                 BucketName = s3bucket,
@@ -166,8 +274,8 @@ public static class HttpImageTrigger
                 // Clean up stream
                 stream.Dispose();
 
-                return new Response(true, fileName + " uploaded to S3 successfully\n\n" +
-                "https://urltogohere/" + s3bucket + "/" + fileName);
+                return new Response(true, "Uploaded to S3 successfully:\n\n" +
+                $"https://{s3bucket}.s3.ap-southeast-2.amazonaws.com/{fileName}\n");
             }
             else
             {
@@ -188,19 +296,41 @@ public static class HttpImageTrigger
     }
 
     // Check if image already exists on S3, returns true if exists
-    public static async Task<Response> imageAlreadyExistsOnS3(string fileName)
+    public static async Task<Response> ImageAlreadyExistsOnS3(string fileName, ILogger log)
     {
         try
         {
             var response = await s3client!.GetObjectAsync(bucketName: s3bucket, key: fileName);
             if (response.HttpStatusCode == HttpStatusCode.OK)
-                return new Response(true, fileName + " already exists on S3 bucket");
+            {
+                string msg = fileName + " already exists on S3 bucket";
+                log.LogInformation(msg);
+                return new Response(true, msg);
+            }
             else
-                return new Response(false, "");
+            {
+                log.LogWarning("Received abnormal code from S3: " +
+                response.HttpStatusCode.ToString());
+                return new Response(false);
+            }
+        }
+        catch (Amazon.S3.AmazonS3Exception e)
+        {
+            if (e.Message.StartsWith("The specified key does not exist."))
+            {
+                log.LogWarning("Key doesn't yet exist:" + fileName + "\n" + e.Message);
+                return new Response(false);
+            }
+            else
+            {
+                log.LogError(e.Message);
+                return new Response(true, e.ToString());
+            }
         }
         catch (System.Exception e)
         {
-            // If an exception occurs, return true which will end the program and display the error
+            // Return true which will end the program and display the error
+            log.LogError(e.Message);
             return new Response(true, e.ToString());
         }
     }
@@ -219,14 +349,20 @@ public static class HttpImageTrigger
     }
 }
 
+// Response class used by functions that return to the main Run task
+// Always return true/false whether the function succeeded, 
+// and optionally provide a message and byte[] payload.
 public class Response
 {
     public bool Succeeded;
     public string Message;
-    public Response(bool Succeeded, string Message)
+    public byte[] bytePayload;
+
+    public Response(bool Succeeded, string Message = "", byte[] bytePayload = null)
     {
         this.Succeeded = Succeeded;
         this.Message = Message;
+        this.bytePayload = bytePayload;
     }
 }
 
--- Readme.md
+++ Readme.md
@@ -1,20 +1,46 @@
 # Image Transparent to S3 Func
 
-This is an Azure Serverless Function that takes a url of an opaque image, converts it to a transparent webp image format, and then stores the result in AWS S3 bucket.
+This is an Azure Serverless Function that takes a url of an opaque image, converts it to a transparent webp image format, and then stores the result in an AWS S3 bucket.
 
-The function runs on Azure for its ease-of-use over Lambda, and images are stored on AWS S3 over Azure Storage for its better pricing structure.
+The function runs on a mix of Azure and AWS.
+Azure Functions is used for its ease-of-use over Lambda, and images are stored on AWS S3 over Azure Storage for its better pricing structure.
 
-The function is triggered with by a HTTP Trigger, and requires `id` and `url` query parameters to be set.
-`id` represents the filename to be saved in S3. It shouldn't include a file extension.
+## Setup
 
-Example:
+Requires an Azure Storage Account used for temporary storage to run the Azure Function.
 
-```http
-/HttpImageTrigger?id=12345&url=http://domain.com/filename.jpg
+For AWS, it is recommended to create a new user with minimum permission to read and write to S3.
+
+To run locally, download project and create a `local.settings.json` with the following:  
+
+```json
+{
+  "IsEncrypted": false,
+  "Values": {
+    "AzureWebJobsStorage": "<Your Azure Storage Connection String>",
+    "FUNCTIONS_WORKER_RUNTIME": "dotnet",
+    "AWS_ACCESS_KEY": "<Your AWS Access Key>",
+    "AWS_SECRET_KEY": "<Your AWS Secret Key>"
+  }
+}
 ```
 
-Azure Functions require an azure storage connection set.
+To deploy to an Azure Function, set the Application Settings for `AzureWebJobsStorage`, `AWS_ACCESS_KEY`, and `AWS_SECRET_KEY`.
 
-Add image conversion stats.
+## Usage
 
-Add more query parameters for resizing, quality.
+The function is triggered by http GET request. It requires `fileName` and `url` query parameters to be set.
+`fileName` represents the filename to be saved in S3, and any .extension will be replaced with .webp format.
+
+Example Usage:
+
+```html
+/HttpImageTrigger?id=12345&url=http://domain.com/filename.png
+```
+
+Output:
+
+```cmd
+12345.webp
+todo with sample images
+```
