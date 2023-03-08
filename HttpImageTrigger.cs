8ae60e5e9e0ed9696c91df1f1b65c8efb6142175
Jason Burgess <jason.nzd@gmail.com>
2023-03-08 13:28:01 +1300

    switched function responses from bools to objects

--- HttpImageTrigger.cs
+++ HttpImageTrigger.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Logging;
 using Amazon;
 using Amazon.S3;
 using Amazon.S3.Model;
+using Amazon.Runtime;
 using ImageMagick;
 
 public static class HttpImageTrigger
@@ -28,24 +30,30 @@ public static class HttpImageTrigger
         string id = req.Query["id"];
         string url = req.Query["url"];
 
-        // Check query parameters are valid
+        // If query parameters are not valid, return error Message
         if (id == null || url == null || id.Length < 3 || !url.Contains("http"))
+        {
             return new OkObjectResult(
                 "This function requires 'id' and 'url' query parameters\n " +
                 "example: /HttpImageTrigger?id=12345&url=http://domain.com/filename.jpg"
             );
+        }
+
+        // Establish connection to S3
+        var connectResponse = ConnectToS3();
+        if (!connectResponse.Succeeded)
+            return new BadRequestObjectResult(connectResponse.Message);
+
+        // Init httpclient, s3client, out stream
+        httpclient = new HttpClient();
+        Stream outStream = new MemoryStream();
 
         // Derive filename to be written to S3 from id
         string fileName = id + ".webp";
 
-        // Init httpclient, s3client, out stream
-        httpclient = new HttpClient();
-        s3client = new AmazonS3Client(RegionEndpoint.APSoutheast2);
-        Stream outStream = new MemoryStream();
-
         // Check S3 if file already exists
-        if (await imageAlreadyExistsOnS3(fileName))
-            return new OkObjectResult(fileName + " already exists on S3 bucket");
+        var existsResponse = await imageAlreadyExistsOnS3(fileName);
+        if (existsResponse.Succeeded) return new OkObjectResult(existsResponse.Message);
 
         // If it doesn't exist, download from url as bytes[]
         byte[] downloadedBytes;
@@ -60,20 +68,47 @@ public static class HttpImageTrigger
             e.Message + e.ToString());
         }
 
+        string consolidatedMessage = "";
+
         // Load stream into ImageMagick, convert to transparent 180px webp image
-        bool imResponse = MakeImageTransparent(downloadedBytes, outStream, log);
-        if (!imResponse)
-            return new BadRequestObjectResult(fileName + " was unable to be processed by ImageMagick");
+        var imResponse = MakeImageTransparent(downloadedBytes, outStream, log);
+        if (imResponse.Succeeded)
+            consolidatedMessage += imResponse.Message + "\n";
+        else
+            return new BadRequestObjectResult(imResponse.Message);
 
         // Upload stream to S3
-        bool s3Response = await UploadStreamToS3(fileName, outStream, log);
-        if (s3Response)
-            return new OkObjectResult(fileName + " uploaded to S3 successfully");
+        var s3Response = await UploadStreamToS3(fileName, outStream, log);
+        if (s3Response.Succeeded)
+            return new OkObjectResult(consolidatedMessage + s3Response.Message);
         else
-            return new BadRequestObjectResult(fileName + " was unable to be downloaded");
+            return new BadRequestObjectResult(s3Response.Message);
     }
 
-    private static bool MakeImageTransparent(byte[] input, Stream outStream, ILogger log)
+    private static Response ConnectToS3()
+    {
+        try
+        {
+            // Connect to S3 using credentials from env
+            string accessKey = Environment.GetEnvironmentVariable("AWSAccessKey");
+            string secretKey = Environment.GetEnvironmentVariable("AWSSecretKey");
+            BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+            s3client = new AmazonS3Client(credentials, RegionEndpoint.APSoutheast2);
+
+            return new Response(true, "");
+        }
+        catch (System.Exception)
+        {
+            return new Response(
+                false,
+                "Unable to connect to S3. Check credentials were set as environment settings:\n" +
+                "\"AWSAccessKey\": \"<your aws access key>\",\n" +
+                "\"AWSSecretKey\": \"<your aws secret key>\""
+            );
+        }
+    }
+
+    private static Response MakeImageTransparent(byte[] input, Stream outStream, ILogger log)
     {
         try
         {
@@ -88,32 +123,32 @@ public static class HttpImageTrigger
                 image.FloodFill(MagickColors.Transparent, 1, 1);
                 image.Shave(1, 1);
 
-                // Sets the output format to 180x180 webp
-                image.Scale(180, 180);
-                image.Quality = 60;
+                // Sets the output format to 200x200 webp
+                image.Scale(200, 200);
+                image.Quality = 75;
                 image.Format = MagickFormat.WebP;
 
                 // Write the image to outStream
                 image.Write(outStream);
 
-                // If image conversion is successful, log message
+                // If image conversion is successful, log Message
                 long imageByteLength = outStream.Length;
-                log.LogWarning(
+
+                return new Response(
+                    true,
                     "Image resized - original = " + printFileSize(input.LongLength) +
                     " - thumbnail = " + printFileSize(imageByteLength)
                 );
-
-                return true;
             }
         }
         catch (System.Exception e)
         {
             log.LogError(e.ToString());
-            return false;
+            return new Response(false, "Unable to be processed by ImageMagick");
         }
     }
 
-    private static async Task<bool> UploadStreamToS3(string fileName, Stream stream, ILogger log)
+    private static async Task<Response> UploadStreamToS3(string fileName, Stream stream, ILogger log)
     {
         try
         {
@@ -128,43 +163,45 @@ public static class HttpImageTrigger
 
             if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
             {
-                // Clean up streams
+                // Clean up stream
                 stream.Dispose();
 
-                return true;
+                return new Response(true, fileName + " uploaded to S3 successfully\n\n" +
+                "https://urltogohere/" + s3bucket + "/" + fileName);
             }
             else
             {
                 log.LogError(response.HttpStatusCode.ToString());
-                return false;
+                return new Response(false, fileName + " was unable to be uploaded to S3");
             }
         }
         catch (AmazonS3Exception e)
         {
             log.LogError("S3 Exception: " + e.Message);
-            return false;
+            return new Response(false, e.Message);
         }
         catch (System.Exception e)
         {
             log.LogError(e.Message);
-            return false;
+            return new Response(false, e.Message);
         }
     }
 
     // Check if image already exists on S3, returns true if exists
-    public static async Task<bool> imageAlreadyExistsOnS3(string fileName)
+    public static async Task<Response> imageAlreadyExistsOnS3(string fileName)
     {
         try
         {
             var response = await s3client!.GetObjectAsync(bucketName: s3bucket, key: fileName);
             if (response.HttpStatusCode == HttpStatusCode.OK)
-                return true;
+                return new Response(true, fileName + " already exists on S3 bucket");
             else
-                return false;
+                return new Response(false, "");
         }
-        catch (System.Exception)
+        catch (System.Exception e)
         {
-            return false;
+            // If an exception occurs, return true which will end the program and display the error
+            return new Response(true, e.ToString());
         }
     }
 
@@ -179,6 +216,17 @@ public static class HttpImageTrigger
             return longString.Substring(0, longString.Length - 3) + " KB";
 
         else return longString.Substring(0, longString.Length - 6) + " MB";
+    }
+}
+
+public class Response
+{
+    public bool Succeeded;
+    public string Message;
+    public Response(bool Succeeded, string Message)
+    {
+        this.Succeeded = Succeeded;
+        this.Message = Message;
     }
 }
 
--- Readme.md
+++ Readme.md
@@ -1,6 +1,20 @@
-# Todo
+# Image Transparent to S3 Func
+
+This is an Azure Serverless Function that takes a url of an opaque image, converts it to a transparent webp image format, and then stores the result in AWS S3 bucket.
+
+The function runs on Azure for its ease-of-use over Lambda, and images are stored on AWS S3 over Azure Storage for its better pricing structure.
+
+The function is triggered with by a HTTP Trigger, and requires `id` and `url` query parameters to be set.
+`id` represents the filename to be saved in S3. It shouldn't include a file extension.
+
+Example:
+
+```http
+/HttpImageTrigger?id=12345&url=http://domain.com/filename.jpg
+```
 
 Azure Functions require an azure storage connection set.
 
-AWS S3 requires credentials for S3 PUT Object set.
+Add image conversion stats.
 
+Add more query parameters for resizing, quality.
